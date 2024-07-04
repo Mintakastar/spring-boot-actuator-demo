39325e9f369b2af7a9b75f7f6439c7ec52576ead
Raffenio Lopez <mintaka.star@gmail.com>
2024-07-04 15:20:48 -0700

    initial project

---
+++ src/test/java/com/raffenio/spring/boot/actuator/demo/SpringBootActuatorDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.raffenio.spring.boot.actuator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//this is initial project , will be implemented later
+	}
+
+}
